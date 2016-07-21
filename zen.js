6f28ff532cfb06de34fc124b7dd27677db9ff84d
AbdelMounhim <email@address.com>
2016-07-21 16:08:31 +0200

    premier commit

---
+++ zen.js
@@ -0,0 +1,9 @@
+
+
+
+document.getElementsByClassName("btn_lecture_zen btn_offre_decouverte_zen")[0].click();
+
+setTimeout(function() {
+document.getElementById("offre_decouverte_form").style.display="none";
+document.getElementsByClassName("fond_overlay offre_decouverte")[0].style.display="none";	
+},5000) ;
